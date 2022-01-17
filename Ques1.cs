cbdd533e1c2b0e8689c0c7e8aaf266f4ab6de379
akshaypathak2565 <52322903+akshaypathak2565@users.noreply.github.com>
2022-01-17 17:35:35 +0530

    Add files via upload

---
+++ Ques1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assesment2_Ques1
+{
+    public class Trading
+    {      
+    }
+    public class Stocks : Trading
+    {
+        public string Volatility = "Low";     
+    }
+    public class Crypto : Trading
+    {
+        public string Volatility = "High";     
+    }
+    public class BinaryOptions : Trading
+    {
+       public string Volality = "Risk";
+    }
+
+     public class Program
+    {
+     public static void GetVolatility(Trading trading)
+        {
+            if(trading is Stocks stks)
+            {
+                Console.WriteLine($"stks.Volatility");
+            }
+            if(trading is Crypto crypto)
+            {
+                Console.WriteLine($"crypto.Volatility");
+            }
+            if(trading is BinaryOptions boption){
+                Console.WriteLine($"boption.Volatility");
+            }
+            static void Main(string[] args)
+            {
+                Stocks stk = new Stocks();
+                Crypto c = new Crypto();
+                BinaryOptions bo =  new BinaryOptions();
+
+                GetVolatility(stk);
+                GetVolatility(c);
+                GetVolatility(bo);
+            }
+        }
+    }
+}
---
+++ Ques2.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessment2
+{
+    class Employee
+    {
+        int Empno;
+        string Ename;
+        int Deptno;
+        decimal Salary;
+        decimal Commission;
+
+        public void setEmployee(int eno, string ename, int deptno, decimal salary, decimal commission)
+        {
+            this.Empno = eno;
+            this.Ename = ename;
+            this.Deptno = deptno;
+            this.Salary = salary;
+            this.Commission = commission;
+        }
+
+        public void showEmployee()
+        {
+            Console.WriteLine("Employee Details:");
+            Console.WriteLine($"Id: {this.Empno}");
+            Console.WriteLine($"Name: {this.Ename}");
+            Console.WriteLine($"Department No: {this.Deptno}");
+            Console.WriteLine($"Salary : {this.Salary}");
+            Console.WriteLine($"Commission : {this.Commission}");
+        }
+
+        public decimal getTotalSalary()
+        {
+            return this.Salary + this.Commission;
+        }
+
+        public bool isTopEmployee()
+        {
+            return this.Commission > this.Salary ? true : false;
+        }
+    }
+    internal class Question2
+    {
+        public static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+
+
+            Action<int, string, int, decimal, decimal> setEmployee = employee.setEmployee;
+            setEmployee(101, "Harsh Kumar", 3, 10000, 50000);
+
+            Action showEmployee = employee.showEmployee;
+            showEmployee();
+
+            Func<decimal> totalSalary = employee.getTotalSalary;
+            Console.WriteLine($"Total Salary {totalSalary.Invoke()}");
+
+            Func<bool> isTopEmployee = employee.isTopEmployee;
+            bool res = isTopEmployee();
+
+            if (res)
+            {
+                Console.WriteLine("Top Employee");
+            }
+            else
+            {
+                Console.WriteLine("Work hard and smart to become Top Employee");
+            }
+        }
+    }
+}
